5851457adb5545a52c38e400bb7a56426f3c91cf
FedeSeque <fedeseque@yahoo.com.ar>
2012-06-07 13:56:22 -0500

    Several users modifications

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'rails', '3.1.3'
 
 gem 'sqlite3'
 
+gem 'gravatar_image_tag'
+
 group :development do
 	gem 'rspec-rails'
 	gem 'annotate'
@@ -15,6 +17,8 @@ end
 group :test do
 	gem 'rspec', "~> 2.10.0"
 	gem 'webrat'
+	gem 'spork'
+	gem 'factory_girl_rails'
 end
 
 # Gems used only for assets and not required
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -15,5 +15,31 @@ describe UsersController do
 			response.should have_selector("title", :content => "Sign up")
 		end
   end
+  
+  describe "GET 'show'" do
+		before(:each) do
+			@user = FactoryGirl.create(:user)
+		end
+		
+		it "should be successful" do
+			get :show, :id => @user
+			response.should be_success
+		end
+		
+		it "should find the right user" do
+			get :show, :id => @user
+			assigns(:user).should == @user
+		end
+		
+		it "should include the user's name" do
+			get :show, :id => @user
+			response.should have_selector("h1", :content => @user.name)
+		end
+		
+		it "should have a profile image" do
+			get :show, :id => @user
+			response.should have_selector("h1>img", :class => "gravatar")
+		end
+	end
 
 end
